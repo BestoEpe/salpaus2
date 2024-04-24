c42dfaef3e4ed736ebe18ef7165fa07a3125e647
Hakkerikakkeri <88773115+Hakkerikakkeri@users.noreply.github.com>
2024-04-24 06:57:02 +0000

    Update ClickOppilaitos.js

--- ClickOppilaitos.js
+++ ClickOppilaitos.js
@@ -1,4 +1,4 @@
-const { Builder, By, until } = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
 async function clickOppilaitosElement() {
@@ -10,26 +10,23 @@ async function clickOppilaitosElement() {
         await driver.get('https://vipunen.fi/fi-fi/Raportit/Ammatillinen%20koulutus%20-%20ty%C3%B6llistyminen%20ja%20jatko-opinnot%20-%20tilastovuosi.xlsb?Web=1');
         console.log('Page loaded successfully.');
 
-        // Wait for 10 seconds before clicking the Oppilaitos element
         await driver.sleep(10000);
 
-        // Wait for the Oppilaitos element to be located and clickable
         const oppilaitosElement = await driver.wait(until.elementLocated(By.xpath('//*[@id="m_excelWebRenderer_nov_ewaCtl__2_Oppilaitos_21_0"]')), 15000);
         await driver.wait(until.elementIsVisible(oppilaitosElement), 15000);
         await driver.wait(until.elementIsEnabled(oppilaitosElement), 15000);
-        
-        // Click on the Oppilaitos element
+
         await oppilaitosElement.click();
         console.log('Oppilaitos element clicked.');
 
-        // Wait for 15 seconds before scrolling to the "Koulutuskeskus Salpaus" part
-        await driver.sleep(15000);
+        await driver.sleep(10000);
 
+        const adultaElement = await driver.wait(until.elementLocated(By.xpath('//td[@id="ctl00_AdultaOy1"]')), 15000);
+        await driver.wait(until.elementIsVisible(adultaElement), 15000);
+        await driver.wait(until.elementIsEnabled(adultaElement), 15000);
 
-       const koulutuskeskusSalpausElement = await driver.wait(until.elementLocated(By.xpath('//*[@id="ctl00_KoulutuskeskusSalpaus25"]')), 15000);
-       await driver.wait(until.elementIsVisible(oppilaitosElement), 15000);
-       await driver.wait(until.elementIsEnabled(oppilaitosElement), 15000);
-        // You can add further actions here if needed
+        await adultaElement.click();
+        console.log('Adulta element clicked.');
     } catch (error) {
         console.error('Error:', error);
     } finally {
@@ -37,5 +34,4 @@ async function clickOppilaitosElement() {
     }
 }
 
-// Call the function to click the Oppilaitos element
 clickOppilaitosElement();
